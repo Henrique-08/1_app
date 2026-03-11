d77f200811abf33109ad275d62c7e4c82e129663
Carlos Henrique Santana Lemos <00001116925928sp@aluno.educacao.sp.gov.br>
2026-03-11 17:19:34 -0300

    adicionando projeto

---
+++ 1_app1/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _1_app.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
