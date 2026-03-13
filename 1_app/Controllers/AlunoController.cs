3e465a3a44106caddee4acb4006c03d05731888f
Carlos Henrique Santana Lemos <00001116925928sp@aluno.educacao.sp.gov.br>
2026-03-13 15:23:18 -0300

    primeiro commit

--- 1_app/Controllers/AlunoController.cs
+++ 1_app/Controllers/AlunoController.cs
@@ -6,6 +6,8 @@ namespace _1_app.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.Nota = "Aluno nota 10";
+            ViewData["escola"] = "E.E. Anhanguera";
             return View();
         }
     }
